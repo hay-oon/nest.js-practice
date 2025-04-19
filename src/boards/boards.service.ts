deb9e3277c81ae056d170b85f952c642c4a91375
Ripponcheese <hayooncode@gmail.com>
2025-04-19 23:08:29 +0900

    feat: createBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = []; // private : 다른 컴포넌트에서 배열값을 수정할 수 없게 하기 위해 사용
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
