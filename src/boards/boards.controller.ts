78d5743f9d3e524f3725a00aa61fc80e5908a20e
hay-oon <hayooncode@gmail.com>
2025-04-22 16:18:53 +0900

    커스텀 파이프

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,7 +12,7 @@ import {
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { get } from 'http';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -24,7 +24,7 @@ export class BoardsController {
   }
 
   @Post()
-  @UsePipes(ValidationPipe)
+  @UsePipes(ValidationPipe) // 핸들러 레벨의 pipe
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
@@ -42,7 +42,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus, // 파라미터 레벨의 pipe
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} 는 올바른 값이 아닙니다!`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
