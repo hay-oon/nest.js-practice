355fc76c731add06a167986f02ac30154a82a079
hay-oon <hayooncode@gmail.com>
2025-04-25 01:17:10 +0900

    req.user에서  password 제외

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,10 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
-// 커스텀 데코레이터 생성 : req.user 추출
+// 커스텀 데코레이터 생성 : req.user 추출 (password 제외)
 export const GetUser = createParamDecorator(
-  (_data, ctx: ExecutionContext): User => {
+  (_data, ctx: ExecutionContext): Omit<User, 'password'> => {
     const req = ctx.switchToHttp().getRequest();
-    return req.user;
+    const { password, ...userWithoutPassword } = req.user;
+    return userWithoutPassword;
   },
 );
